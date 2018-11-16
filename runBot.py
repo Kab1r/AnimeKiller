a3ae055a04f67bf802df6c2e1dc0253014216910
Kabir <kabirkwatra@techie.com>
2018-11-15 22:52:04 -0800

    runBot edits

--- runBot.py
+++ runBot.py
@@ -1,42 +1,50 @@
-import discord, requests
+import discord
+import requests
 from discord.ext import commands
 from discord.ext.commands import Bot
-from PIL import Image
-from io import BytesIO
 
 import tensorflow as tf
+import numpy as np
+import cv2
+import urllib
 
 # Dataset Imports
 
 # Train Models
 
 # Test Image
+def isAnime(url):
+	req = urllib.request.Request(url)
+	image = urllib.request.urlopen(req).read()
+	#pool3_features = sess.run(pool3,{'incept/DecodeJpeg/contents:0': image.read()})
+	image.resize((299, 299), image.ANTIALIAS)
+	image_array = np.array(image)[:, :, 0:3] # RGB only
 
 # Discord
 
-description = '''An open source solution
-to the anime epidemic on Discord.'''
+descr = 'An open source solution to the anime epidemic on Discord.'
+
 bot = commands.Bot(command_prefix = 'ak!',
-    description = description)
+    description = descr)
 
 picEXT = ['.jpeg', '.png', '.jpg']
 
 # On Ready Function
 @bot.event
 async def on_ready():
-    print('Logged in as')
-    print(bot.user.name + ", " + bot.user.id)
+	print('Logged in as')
+	print(bot.user.name)
+	print('ID:')
+	print(bot.user.id)
 
 @bot.event
 async def on_message(message):
     for ext in picEXT:
         if message.content.endswith(ext):
             url = message.attachments[0]['url']
-            img = Image.open(
-                BytesIO(requests.get(url).content))
-
+            if isAnime(url):
+            	await bot.delete_message(message)
 # Run Discord
 # Gets token from 'token.secret' file
-with open(token.secret) as f:
-    token = f.readlines()
-client.run(token)
+token = open('token.secret', 'r').read()
+bot.run(token)
