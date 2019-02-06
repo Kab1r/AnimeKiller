6db38d69953c6be07e121055eb359905d087951b
Kabir Kwatra <kkwatra1@saddleback.edu>
2019-02-05 23:53:58 -0800

    added docs and removed extra imports

--- runBot.py
+++ runBot.py
@@ -3,91 +3,108 @@ import os.path
 import re
 
 import cv2
-import discord
 from discord.ext import commands
-from discord.ext.commands import Bot
 from google.cloud import vision
 from google.cloud.vision import types
 from google.oauth2 import service_account
 
 from url_to_image import ImageConverter
 
-google_credentials = service_account.Credentials.from_service_account_info(
-    json.loads(os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')))
-visionary = vision.ImageAnnotatorClient(
-    credentials=google_credentials)
+DESCRIPTION = 'An open source solution to the anime epidemic on Discord.'
+
+BOT = commands.Bot(command_prefix='ak!',
+                   description=DESCRIPTION)
+
+PICTURE_EXT = ['.jpeg', '.png', '.jpg', '.gif']
 
 
-def vision_detect(url):
-    image = types.Image(
-        content=cv2.imencode('.jpg', ImageConverter.url_to_cv(url))[1].tostring())
-    response = visionary.label_detection(image=image)
-    labels = response.label_annotations
-    for label in labels:
-        if(label.description.lower() == 'anime'):
-            return label.score
-    return 0
-# Discord
-
-
-descr = 'An open source solution to the anime epidemic on Discord.'
-
-bot = commands.Bot(command_prefix='ak!',
-                   description=descr)
-
-picEXT = ['.jpeg', '.png', '.jpg', '.gif']
-
-# On Ready Function
-
-
-@bot.event
+@BOT.event
 async def on_ready():
+    """
+        Displays a simple
+        message on boot
+    """
     print('Logged in as')
-    print(bot.user.name)
+    print(BOT.user.name)
     print('ID:')
-    print(bot.user.id)
+    print(BOT.user.id)
 
 
-@bot.event
+@BOT.event
 async def on_message(message):
+    """
+        Main trigger for bot,
+        checks message 'unkilled'
+        role. Messages sent by those
+        with the role are ignored
+        from the anime removal.
+    """
     for role in message.author.roles:
         if role.name.lower == 'unkilled':
             return  # Breaks out of function
 
-    for ext in picEXT:
+    for ext in PICTURE_EXT:
         await check_message(message, ext)
 
 
 async def check_message(message, ext):
-    # Looks at each attatchment's URL
+    """
+        Checks each message for urls
+        and each attachment for picture
+        extentions. Sends urls to checker
+    """
     for attachment in message.attachments:
         if attachment.url.endswith(ext):
             await check_url(attachment.url, message)
 
     # Looks at URLS
     urls = re.findall(
-        'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
-        , message.content.lower())
+        'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', message.content.lower())
     for url in urls:
         await check_url(url, message)
 
 
 async def check_url(url, message):
+    """String url -> vission_detect -> delete_message"""
     likelihood = vision_detect(url)
     if likelihood > 0.0:
         await delete_message(likelihood, message)
 
 
+def vision_detect(url):
+    """String url -> Google Cloud -> Anime Score"""
+    image = types.Image(
+        content=cv2.imencode('.jpg', ImageConverter.url_to_cv(url))[1].tostring())
+    response = VISIONARY.label_detection(image=image)
+    labels = response.label_annotations
+    for label in labels:
+        if label.description.lower() == 'anime':
+            return label.score
+    return 0
+
+
 async def delete_message(likelihood, message):
+    """
+        Deletes given message,
+        sends alert with likelihood
+    """
     await message.delete()
     await message.channel.send(
         "Image containing anime was deleted with {0}% certainty".format(
             '{0:.2f}'.format(likelihood*100))
     )
-# Run Discord
-# Gets token from 'token.secret' file or Heroku
-if (os.environ.get('IS_HEROKU', None)):
-    token = os.environ.get('TOKEN', None)
-else:
-    token = open('token.secret', 'r').read()
-bot.run(token)
+# Creates Vission Client
+# Gets token and credentials from Env Var
+
+VISIONARY = vision.ImageAnnotatorClient(
+    credentials=service_account.Credentials.from_service_account_info(
+        json.loads(
+            os.environ.get(
+                'GOOGLE_APPLICATION_CREDENTIALS'
+            )
+        )
+    )
+)
+
+TOKEN = os.environ.get('TOKEN')
+BOT.run(TOKEN)
